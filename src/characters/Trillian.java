44401214f882ee4345a222fea549f6d4d561d957
carengyl <pavnikol84@gmail.com>
2025-02-21 19:20:20 +0300

    Add some Actions to Characters

--- src/characters/Trillian.java
+++ src/characters/Trillian.java
@@ -1,5 +1,30 @@
 package src.characters;
 
-public class Trillian {
-    
+
+import src.actions.Action;
+import src.actions.Knock;
+import src.locations.Place;
+
+public class Trillian extends Character {
+    private int patience;
+
+    public Trillian(Place place) {
+        super(place);
+        this.patience = 100;
+    }
+
+    public float calculateOdds(int oddsFor, int oddsAgainst) {
+        return oddsFor/(oddsFor + oddsAgainst);
+    }
+
+    @Override
+    public void react(Action stimulus) {
+        if (stimulus.getClass().equals(Knock.class) && this.patience > 0) {
+            this.patience -= 5;
+        } else {
+            if (this.patience <= 0) {
+                
+            }
+        }
+    }
 }
--- src/characters/Zafod.java
+++ src/characters/Zafod.java
@@ -1,5 +1,24 @@
 package src.characters;
 
-public class Zafod {
+import src.actions.Action;
+import src.locations.Place;
+
+public class Zafod extends Character implements Inept {
+
+    public Zafod(Place place) {
+        super(place);
+    }
+
+    @Override
+    public void accidentalAction() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'accidentalAction'");
+    }
+
+    @Override
+        public void react(Action action) {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'react'");
+        }
     
 }
